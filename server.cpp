3bc59c96904f48d1323d97302288a044fde8ca21
mjcunninghm <matthew-j-cunningham@uiowa.edu>
2021-09-29 17:15:44 -0500

    server recommit

--- server.cpp
+++ server.cpp
@@ -1,69 +1,62 @@
-// Server side C/C++ program to demonstrate Socket programming
-#include <unistd.h>
 #include <stdio.h>
-#include <sys/socket.h>
 #include <stdlib.h>
-#include <netinet/in.h>
+#include <unistd.h>
 #include <string.h>
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb/stb_image_write.h"
-#include <librealsense2/rs.hpp>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <iostream>
+#include "rs232.h"
+#include <librealsense2/rs.hpp>
 
-#define PORT 8080
-int main(int argc, char const *argv[])
-{
-    int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    rs2::video_frame *other_frame;
-    const char *hello = "Hello from server";
+#define PORT     8080
+#define IP "192.168.137.210"
+
+// Driver code
+int main() {
+    int sockfd;
     int count = 0;
+    rs2::video_frame * buffer;
+    const char *hello = "Hello from server";
+    struct sockaddr_in servaddr, cliaddr;
 
     // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 8080
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-                   &opt, sizeof(opt)))
-    {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
 
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address,
-            sizeof(address))<0)
+    // Filling server information
+    servaddr.sin_family    = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = inet_addr(IP);
+    servaddr.sin_port = htons(PORT);
+
+    // Bind the socket with the server address
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+            sizeof(servaddr)) < 0 )
     {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
+
     while(true){
-        if (listen(server_fd, 3) < 0)
-        {
-            perror("listen");
-            exit(EXIT_FAILURE);
-        }
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
-                (socklen_t*)&addrlen))<0)
-        {
-            perror("accept");
-            exit(EXIT_FAILURE);
-        }
-        read( new_socket , other_frame, 1024);
-        std::cout << (*other_frame).get_data() << std::endl;
-        //stbi_write_png("socket.png", other_frame->get_width(), other_frame->get_height(),
-                //other_frame->get_bytes_per_pixel(), other_frame->get_data(), other_frame->get_stride_in_bytes());
-        printf("\n%d\n", count);
-        count++;
+        int len, n;
+
+        len = sizeof(cliaddr);  //len is value/resuslt
+        printf("Waiting...\n");
+        recvfrom(sockfd, buffer, 921600,
+                 MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+                         reinterpret_cast<socklen_t *>(&len));
+        printf("Client : \n");
+        sendto(sockfd, (const char *)hello, strlen(hello),
+               MSG_DONTWAIT, (const struct sockaddr *) &cliaddr,
+                       len);
+        printf("Hello message sent.\n");
+        std::cout << count++ << std::endl;
     }
     return 0;
 }
