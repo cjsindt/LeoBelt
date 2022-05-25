c18f779a97a470564cf0d50543d58bed9e32ed1b
mjcunninghm <89094719+mjcunninghm@users.noreply.github.com>
2022-05-25 11:34:52 -0500

    update

--- leo-test.cpp
+++ leo-test.cpp
@@ -2,30 +2,23 @@
 // Copyright(c) 2017 Intel Corporation. All Rights Reserved.
 
 #include <librealsense2/rs.hpp>
-//#include "common/example.hpp"
 #include "third-party/imgui/imgui.h" // <imgui.h>
 #include "third-party/imgui/imgui_impl_glfw.h"
 
-//#include <sstream>
 #include <iostream>
-//#include <fstream>
+#include <fstream>
 #include <algorithm>
 #include <cstring>
 #include <string>
 #include <cmath>
-//#include <time.h>
-//#include <iomanip>
 #include <vector>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdint.h>
 #include "rs232.h"
 #include <chrono>
-//#include <math.h>
+#include <ctime>
 
-#define PORT     8080
-#define MAXLINE 1024
-#define serverIP "192.168.137.210"
 #define PI 3.14159265
 
 #define BUF_SIZE 123
@@ -46,7 +39,7 @@ float* findMags(float y_angle, float z_angle, float pixel_dist);
 int main(int argc, char * argv[]) try
 {
     int bdrate = 57600;
-    
+
     char mode[] ={'8','N','1',0}; // 8 data bits, no parity, 1 stop bit
     unsigned char str_recv[BUF_SIZE]; // recv data buffer
     std::string intensityValue = "";
@@ -75,12 +68,12 @@ int main(int argc, char * argv[]) try
             intensityValue = "";
         }
     }
-    
+
     if(RS232_OpenComport(cport_nr, bdrate, mode)) {
         printf("Can not open comport\n");
         //return(0);
     }
-    
+
     // Rapid buzz to indicate device is on and all feathers are functioning
     for (int i=1; i<=8; i++) {
         RS232_cputs(cport_nr, str_send[256*i-1]);
@@ -88,12 +81,12 @@ int main(int argc, char * argv[]) try
         RS232_cputs(cport_nr,str_send[256*(i-1)]);
         sleep(.25); // .25s
     }
-    
+
     // Create a pipeline to easily configure and start the camera
     rs2::config cfg;
-    cfg.enable_stream(RS2_STREAM_DEPTH, -1, 848, 480, RS2_FORMAT_ANY, 60);
+    cfg.enable_stream(RS2_STREAM_DEPTH, -1, 640, 480, RS2_FORMAT_ANY, 30);
     rs2::pipeline pipe;
-    
+
     //The start function returns the pipeline profile which the pipeline used to start the device
     rs2::pipeline_profile profile = pipe.start(cfg);
 
@@ -103,27 +96,24 @@ int main(int argc, char * argv[]) try
 
     rs2::frameset frameset;
     int* c;
-    
-    auto start = std::chrono::high_resolution_clock::now();
-    auto stop = std::chrono::high_resolution_clock::now();
-    
-    //std::ofstream out("/home/pi/leo_belt/error.txt");
-    //std::cout.rdbuf(out.rdbuf());
 
-    while (getFrame < 1000) // Application still alive?
+    auto start = std::chrono::high_resolution_clock::now();
+    int duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()-start).count();
+    std::time_t start_time = std::chrono::system_clock::to_time_t(start);
+    int minute = 0;
+
+    std::ofstream out("/home/pi/leo_belt/log.txt", std::ios::app);
+    std::cout.rdbuf(out.rdbuf());
+
+    std::cout << "\nProgram start: " << std::ctime(&start_time);
+
+    while (true) // Application still alive?
     {
         try {
             // Using the align object, we block the application until a frameset is available
             frameset = pipe.wait_for_frames(34);
         }
         catch (const std::exception& e) {
-            stop = std::chrono::high_resolution_clock::now();
-            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop-start);
-            std::cout << "(catch block) frames: ";
-            std::cout << getFrame;
-            std::cout << " time: ";
-            std::cout << duration.count() << std::endl;
-            
             continue;
         }
 
@@ -142,30 +132,21 @@ int main(int argc, char * argv[]) try
         {
             continue;
         }
-        
+
         // Obtaining pixel depth
         c = printPixelDepth(aligned_depth_frame, depth_scale);
 
         getFrame++;
-        
-        if (getFrame%1000 == 0) {
-            stop = std::chrono::high_resolution_clock::now();
-            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop-start);
-            std::cout << "frames: ";
-            std::cout << getFrame;
-            std::cout << " time: ";
-            std::cout << duration.count() << std::endl;
+        duration = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now()-start).count();
+
+        if (duration%60 == 0) {
+            if (getFrame > 70) {
+                minute++;
+                std::cout << "minute: " << minute << " fps: " << getFrame / 60 << std::endl;
+                getFrame = 0;
+            }
         }
     }
-
-    stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop-start);
-    std::cout << " time: ";
-    std::cout << duration.count() << std::endl;
-            
-    usleep(1000000);
-    silenceAllFeathers();
-    return EXIT_SUCCESS;
 }
 
 catch (const rs2::error & e)
@@ -193,8 +174,8 @@ void silenceAllFeathers(){
 int* printPixelDepth(const rs2::depth_frame& depth_frame, float depth_scale) {
 
     const uint16_t* p_depth_frame = reinterpret_cast<const uint16_t*>(depth_frame.get_data());
-    
-    int width = 848;
+
+    int width = 640;
     int height = 480;
 
     float closest[8] = {10,10,10,10,10,10,10,10};
@@ -275,116 +256,98 @@ int* printPixelDepth(const rs2::depth_frame& depth_frame, float depth_scale) {
             }
         }
     }
-    
+
     // converting physical distance to 0-255 scale
     for(int i=0; i<=7; i++) {
         closest[i] = 255 + ((0-255.0)*(closest[i]-.001)/(1.413-.001));
         closest[i] = trunc(closest[i]);
     }
-    
+
     // Obtaining (R)G(B) color values for each quadrant in heat map
     for(int i=0; i<=7; i++) {
         if(closest[i]<=255 && closest[i]>=0) {
             color[i] = closest[i];
             color[i+8] = 255 - closest[i];
         }
-        // Anything greater than 2m away will remain blue on map
+            // Anything greater than 2m away will remain blue on map
         else {
             color[i] = 0;
             color[i+8] = 255;
         }
-        //std::cout << "red: " << color[i] << " blue: " << color[i+8] << std::endl;
     }
-    
+
     //Feather 1
-    //std::cout << "Quadrant 1 closest: " << closest[0] << "m -> Intensity: ";
-    //printf("Quadrant 1 closest: %.3fm Intensity: ", closest[0]);
     if(closest[0] <= 255 && closest[0] >= 0) {
         int n = closest[0];
-        //std::cout << "feather 1: " << n << " " << str_send[n] << std::endl;
         RS232_cputs(cport_nr, str_send[n]);
     }
     else {
         RS232_cputs(cport_nr, str_send[0]);
-	}
-    
+    }
+
     //Feather 2
-    //printf("Quadrant 2 closest: %.3fm Intensity: ", closest[1]);
     if(closest[1] <= 255 && closest[1] >= 0) {
         int n = closest[1] + 256;
-        //std::cout << "feather 2: " << n << " " << str_send[n] << std::endl;
-		RS232_cputs(cport_nr, str_send[n]);
-	}
+        RS232_cputs(cport_nr, str_send[n]);
+    }
     else {
         RS232_cputs(cport_nr, str_send[0+256]);
-	}
-    
+    }
+
     //Feather 3
-    //printf("Quadrant 3 closest: %.3fm Intensity: ", closest[2]);
     if(closest[2] <= 255 && closest[2] >= 0) {
         int n = closest[2] + 2*256;
-        //std::cout << "feather 3: " << n << " " << str_send[n] << std::endl;
-		RS232_cputs(cport_nr, str_send[n]);
-	}
+        RS232_cputs(cport_nr, str_send[n]);
+    }
     else {
         RS232_cputs(cport_nr, str_send[0+(2*256)]);
-	}
-    
+    }
+
     //Feather 4
-    //printf("Quadrant 4 closest: %.3fm Intensity: ", closest[3]);
     if(closest[3] <= 255 && closest[3] >= 0) {
         int n = closest[3] + 3*256;
-        //std::cout << "feather 4: " << n << " " << str_send[n] << std::endl;
-		RS232_cputs(cport_nr, str_send[n]);
-	}
+        RS232_cputs(cport_nr, str_send[n]);
+    }
     else {
         RS232_cputs(cport_nr, str_send[0+(3*256)]);
-	}
-    
+    }
+
     //Feather 5
-    //printf("Quadrant 5 closest: %.3fm Intensity: ", closest[4]);
     if(closest[4] <= 255 && closest[4] >= 0) {
         int n = closest[4] + 4*256;
-        //std::cout << "feather 5: " << n << " " << str_send[n] << std::endl;
-		RS232_cputs(cport_nr, str_send[n]);
-	}
+        RS232_cputs(cport_nr, str_send[n]);
+    }
     else {
         RS232_cputs(cport_nr, str_send[0+(4*256)]);
-	}
-    
+    }
+
     //Feather 6
-    //printf("Quadrant 6 closest: %.3fm Intensity: ", closest[5]);
     if(closest[5] <= 255 && closest[5] >= 0) {
         int n = closest[5] + 5*256;
-        //std::cout << "feather 6: " << n << " " << str_send[n] << std::endl;
-		RS232_cputs(cport_nr, str_send[n]);
-	}
+        RS232_cputs(cport_nr, str_send[n]);
+    }
     else {
         RS232_cputs(cport_nr, str_send[0+(5*256)]);
-	}
-    
+    }
+
     //Feather 7
-    //printf("Quadrant 7 closest: %.3fm Intensity: ", closest[6]);
     if(closest[6] <= 255 && closest[6] >= 0) {
         int n = closest[6] + 6*256;
-        //std::cout << "feather 7: " << n << " " << str_send[n] << std::endl;
-		RS232_cputs(cport_nr, str_send[n]);
-	}
+        RS232_cputs(cport_nr, str_send[n]);
+    }
     else {
         RS232_cputs(cport_nr, str_send[0+(6*256)]);
-	}
-    
+    }
+
     //Feather 8
-    //printf("Quadrant 8 closest: %.3fm Intensity: ", closest[7]);
     if(closest[7] <= 255 && closest[7] >= 0) {
         int n = closest[7] + 7*256;
-        //std::cout << "feather 8: " << n << " " << str_send[n] << std::endl;
-		RS232_cputs(cport_nr, str_send[n]);
-	}
+        RS232_cputs(cport_nr, str_send[n]);
+    }
     else {
         RS232_cputs(cport_nr, str_send[0+(7*256)]);
-	}    
-    
+    }
+
     return color;
 }
 
